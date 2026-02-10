5161f051330665692365c5b52acbad4db558559f
mza18 <maximilian.zarna@tu-clausthal.de>
2026-02-10 19:25:03 +0100

    feat: Implemented function to split text_nodes on given delimiter && Implement function to extract image alt text and url from md-text

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,7 @@
 from textnode import *
 from htmlnode import *
+import re
+
 def text_node_to_html_node(text_node):
     if text_node.text_type not in TextType:
         raise Exception("Invalid text type")
@@ -16,6 +18,49 @@ def text_node_to_html_node(text_node):
     elif text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     return None
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if not old_node.text_type == TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        if delimiter not in old_node.text:
+            raise Exception("Invalid Markdown-Syntax")
+
+        split_text = old_node.text.split(delimiter)
+        if len(split_text) == 0:
+            continue
+
+        for i in range(0, len(split_text)):
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_text[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(split_text[i], text_type))
+    return new_nodes
+
+def extract_markdown_images(text):
+    matches = []
+
+    alt_text = re.findall(r"!\[(.*?)\]", text)
+    urls= re.findall(r"\((\w+:\/\/.*?)\)", text)
+
+    for i in range(0, len(alt_text)):
+        matches.append((alt_text[i], urls[i]))
+
+    return matches
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/test_functions.py
+++ src/test_functions.py
@@ -8,5 +8,45 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+
+    def test_delimiter_split(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE),
+                         [
+                             TextNode("This is text with a ", TextType.TEXT),
+                             TextNode("code block", TextType.CODE),
+                             TextNode(" word", TextType.TEXT),
+                         ]
+                         )
+    def test_delimiter_split_bold(self):
+        node = TextNode("This is text with a **bold block** word and a **second bold block** right here", TextType.TEXT)
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD),
+                         [
+                             TextNode("This is text with a ", TextType.TEXT),
+                             TextNode("bold block", TextType.BOLD),
+                             TextNode(" word and a ", TextType.TEXT),
+                             TextNode("second bold block", TextType.BOLD),
+                             TextNode(" right here", TextType.TEXT),
+                         ]
+                         )
+    def test_delimiter_split_no_text(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE),
+                         [TextNode("This is a text node", TextType.ITALIC)]
+                         )
+
+    def test_delimiter_split_no_delimiter(self):
+        node = TextNode("This is __a italic__ text node", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+
 if __name__ == '__main__':
     unittest.main()
